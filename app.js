b3e9fc76b6f869cb09db5d30900a80d210b8becd
fatimazaidi <f.zaidi182@gmail.com>
2023-12-14 16:40:29 +0000

    add workshop

---
+++ app.js
@@ -0,0 +1,26 @@
+async function getDataFromAPI() {
+    const apiResponse = await fetch ("https://jsonplaceholder.typicode.com/todos");
+    const json = await apiResponse.json();
+    console.log(json);
+}
+
+getDataFromAPI();
+
+
+async function getDataFromGitHub() {
+    const GitHubData = await fetch ("https://api.github.com/repos/fzaidi96/spaceYtask");
+    const json = await GitHubData.json();
+    return json;
+}
+getDataFromGitHub();
+
+async function displayStargazerCount() {
+    const jsonifieddata = await getDataFromGitHub();
+    const p = document.createElement("p");
+    p.textContent = jsonifieddata.stargazer_count;
+    document.body.appendChild(p);
+    }
+
+displayStargazerCount();
+
+// need help on this!?//
